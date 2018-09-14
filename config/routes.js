4dc66e161339464af10d388dfdf70044cdf8d577
Lauren <llwor94@gmail.com>
2018-09-14 00:30:55 -0700

    all routes work, created react app

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const jwtKey = require('../_secrets/keys').jwtKey;
 const db = require('../database/dbConfig');
 const { authenticate } = require('./middlewares');
 
@@ -9,6 +10,18 @@ module.exports = server => {
   server.post('/api/login', login);
   server.get('/api/jokes', authenticate, getJokes);
 };
+
+function generateToken(id) {
+  const payload = {
+    id,
+  };
+  const options = {
+    expiresIn: '1h',
+    jwtid: '12345',
+    subject: `${id}`,
+  };
+  return jwt.sign(payload, jwtKey, options);
+}
 
 function register(req, res) {
   let body = req.body;
@@ -26,7 +39,19 @@ function register(req, res) {
 }
 
 function login(req, res) {
-  // implement user login
+  let body = req.body;
+  if (!(body.username && body.password)) return res.json({ code: 400 });
+  db('users')
+    .where({ username: body.username })
+    .first()
+    .then(response => {
+      if (response && bcrypt.compareSync(body.password, response.password)) {
+        const token = generateToken(response);
+        if (token) return res.status(200).send(token);
+        else res.send({ code: 400 });
+      }
+    })
+    .catch(err => res.status(500).send(err));
 }
 
 function getJokes(req, res) {
